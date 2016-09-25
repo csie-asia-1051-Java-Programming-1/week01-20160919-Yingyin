f615c0fe09e303cfa98276043de6f148382e2067
Yingyin <105791120@live.asia.edu.tw>
2016-09-25 14:58:07 +0800

    week04 杨钰莹 105791120

---
+++ week04/src/week04/week04.java
@@ -0,0 +1,15 @@
+package week04;
+
+import java.util.Scanner;
+
+public class week04 {
+
+	public static void main(String[] args) {
+	Scanner sca = new Scanner(System.in);
+	double a,b;
+	a=sca.nextDouble();
+	b=sca.nextDouble();
+	System.out.print(Math.pow(a,1/b));
+	}
+
+}
