ec57c5741152b763b614923ef4ac9a4fc9ce57d3
Yingyin <105791120@live.asia.edu.tw>
2016-09-25 14:33:53 +0800

    ex04 杨钰莹 105791120

---
+++ ex04/src/ex04/week04.java
@@ -0,0 +1,17 @@
+package ex04;
+
+import java.util.Scanner;
+
+public class week04 {
+
+	public static void main(String[] args) {
+		Scanner sca = new Scanner (System.in);
+		System.out.println("請輸入a");
+	    double a;
+	    a=sca.nextDouble();
+	    double b;
+	    b=Math.sqrt(a);
+	    System.out.print("運算後為："+b);
+	}
+
+}
